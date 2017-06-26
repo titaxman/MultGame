7717b45281ff59a8a44a7ca8a9b8e5925e708b79
gustavohmsilva <ilha02@ILHA02.local>
2017-06-26 10:27:22 -0300

    Small Game Improvements

--- multgame.c
+++ multgame.c
@@ -5,8 +5,9 @@
 void intro(){
   printf("\n\n\n\n\n\n\t\t=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
   printf("\n\t\t=\tMultiplication Table Game\t=\n");
-  printf("\t\t=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+  printf("\t\t=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n");
   printf("Developed by Gustavo H M Silva\nReach me at gustavohmsilva[at]member.fsf.org\nThis game is under BY-CC Creative Commons Attribution 4.0 International License.\nVersion 0.1\n\n");
+  printf("MultGame is a small game programmed in C to help \npractice memorization of the Multiplication game. \nThis small program will add to your points every \nright answer and lower in 10 points every wrong one.\n\n");
 }
 
 int main(){
@@ -29,6 +30,7 @@ int main(){
         points+=res;
       }else{
         printf("\nYour points: %d\t|\tSorry %d X %d is equal to %d, but your answer was %d...\n\n", points, alg1, alg2, alg1*alg2, res);
+        (points > 10) ? (points = points - 10) : (points = 0);
       }
       printf("Your points: %d\t|\tDo you wish to play again (Y or N)? ", points); scanf(" %c", &EscOp);
       (EscOp == 'n' || EscOp =='N') ? (contJog = 0) : (contJog = 1);
